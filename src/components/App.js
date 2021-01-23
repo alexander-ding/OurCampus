a4b4fbe135ae935721029c5afee0361fbee3ed87
ns90001 <63938349+ns90001@users.noreply.github.com>
2021-01-23 14:06:46 -0500

    Added basic homepage

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { hot } from "react-hot-loader/root";
 import { Route, Switch } from "react-router-dom";
 import { compose } from "redux";
+import Home from './Home'
 
 /**
  * The main app, including the primary router and support
@@ -11,7 +12,7 @@ const App = () => (
   <Suspense fallback={<div>Loading</div>}>
     {/** Primary routing */}
     <Switch>
-      <Route path='/'>home</Route>
+      <Route path='/' component={Home} />
       <Route path='*'>
         404
       </Route>
