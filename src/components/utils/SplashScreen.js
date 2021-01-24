a89837e4694ac12f99f08c61bbe20634c8faa51d
Alex Ding <alexander_ding@brown.edu>
2021-01-24 11:30:57 -0500

    Splash screen + favicon

--- src/components/utils/SplashScreen.js
+++ src/components/utils/SplashScreen.js
@@ -2,7 +2,17 @@ import React from "react";
 
 const SplashScreen = () => {
   return <div className="splash-screen">
-    Loading...
+    <div class="preloader-wrapper big active">
+      <div class="spinner-layer spinner-blue-only">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div><div class="gap-patch">
+          <div class="circle"></div>
+        </div><div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+    </div>
   </div>;
 }
 
